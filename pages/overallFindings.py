9c66ce5f4ca4bf3e73aaf7f6cfae73830c559406
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-02 13:26:32 -0700

    tesdjf

---
+++ pages/overallFindings.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+
+
+
+st.set_page_config(page_title="Short Question Responses", page_icon="📈")
+
+st.markdown("# Short Question Response")
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!""" )
+
