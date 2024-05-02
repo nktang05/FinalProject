3dec942fe873619b18e4d5cac4496a203acd37e9
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-01 21:57:21 -0700

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import loadData
+import pandas as pd
+import sqlite3
 
 
 
@@ -10,3 +12,18 @@ print("hi")
 loadData.get_state_data()
 
 print("data loaded")
+
+conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+cur = conn.cursor()
+
+
+sql_query9 = "SELECT Name From states Where Region = 'West' "
+
+# Execute the SQL query and load the results into a DataFrame
+data9 = pd.read_sql(sql_query9, conn)
+
+print(data9)
+st.write(data9)
+
+conn.commit()
+conn.close()
