345cc27822d511b8d793379a98f480c5d4bb55f8
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-02 13:04:27 -0700

    test

---
+++ union.py
@@ -0,0 +1,79 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+import loadData
+import menu
+import grocery
+import obesity
+import population
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+from bs4 import BeautifulSoup
+
+def unionGraphs():
+    graph = menu.menuUnion()
+
+    selectFood, selectRegion = menu.menugrocery()
+
+
+    seriesDict = grocery.getSeriesId(selectFood, selectRegion)
+    optionsList = list(seriesDict.values())
+    option = st.selectbox(
+    'What option would you like?',
+    optionsList)
+
+
+    selected_key = next(key for key, value in seriesDict.items() if value == option)
+    food = grocery.grocery(selected_key, seriesDict)
+
+    obe = obesity.obesityRegion(selectRegion)
+
+
+    pop = population.singlePop(selectRegion)
+
+    food_price_avg = food.groupby("Year")['Price'].mean()
+
+
+
+    fig, ax = plt.subplots()
+
+    if (graph == "Food Prices and Obesity Rates"):
+
+        line1, = ax.plot(food_price_avg.index, food_price_avg, linestyle = "--", color = "blue", label='Price')
+        ax.set_xlabel('Years')
+        ax.set_ylabel('Food Prices')
+
+        ax2 = ax.twinx()
+        line2,  = ax2.plot(obe['Year'], obe['Avg_Obesity_Rate'], label='Obesity Rate', color = "red")
+        ax2.set_ylabel('Obesity Rates')
+
+    elif(graph == "Food Prices and Population"):
+
+        line1, = ax.plot(food_price_avg.index, food_price_avg, linestyle = "--", color = "blue", label='Price')
+        ax.set_xlabel('Years')
+        ax.set_ylabel('Food Prices')
+
+        ax2 = ax.twinx()
+        line2,  = ax2.plot(pop["Year"], pop['avg_population'], label = 'Population', color = "green")
+        ax2.set_ylabel('Population')
+
+    elif(graph == "Population and Obesity Rates"):
+        line1,  = ax.plot(obe['Year'], obe['Avg_Obesity_Rate'], label='Obesity Rate', color = "red")
+        ax.set_xlabel('Years')
+        ax.set_ylabel('Obesity Rates')
+
+        ax2 = ax.twinx()
+        line2,  = ax2.plot(pop["Year"], pop['avg_population'], label = 'Population', color = "green")
+        ax2.set_ylabel('Population')
+
+
+    #plot food and obesity
+    ax2.legend(handles=[line1, line2])
+
+    plt.tight_layout()
+    plt.title('2 plots')
+
+    st.pyplot(fig)
