84debc01d99ad1cb8ef1c5490ea1570d27e0cfc4
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-02 11:37:50 -0700

    test

--- main.py
+++ main.py
@@ -61,13 +61,14 @@ elif(graph == "Food Prices and Population"):
     ax2.set_ylabel('Population')
 
 elif(graph == "Population and Obesity Rates"):
-    line1,  = ax.plot(obe['Year'], obe['Avg_Obesity_Rate'], label='Obesity Rate', color = "red")
+    #line1,  = ax.plot(obe['Year'], obe['Avg_Obesity_Rate'], label='Obesity Rate', color = "red")
+    line1,  = ax.plot(pop["Year"], pop['avg_population'], label = 'Population', color = "green")
     ax.set_xlabel('Years')
     ax.set_ylabel('Obesity Rates')
 
     ax2 = ax.twinx()
-    #line2,  = ax2.plot(pop["Year"], pop['avg_population'], label = 'Population', color = "green")
-    line2,  = ax2.plot(obe['Year'], obe['Avg_Obesity_Rate'], label='Obesity Rate', color = "green")
+    line2,  = ax2.plot(pop["Year"], pop['avg_population'], label = 'Population', color = "green")
+    #line2,  = ax2.plot(obe['Year'], obe['Avg_Obesity_Rate'], label='Obesity Rate', color = "green")
     ax2.set_ylabel('Population')
 
 
