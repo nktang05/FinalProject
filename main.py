2d0ac504bf16857c823b472c63598b1f721f5168
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-01 22:04:14 -0700

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import loadData
 import pandas as pd
 import sqlite3
+import loadObesity
 
 
 
@@ -9,15 +10,15 @@ st.write("hello World")
 print("hi")
 
 
-loadData.get_state_data()
-
 print("data loaded")
+
+loadObesity.get_obesity_data()
 
 conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 cur = conn.cursor()
 
 
-sql_query9 = "SELECT Name From states Where Region = 'West' "
+sql_query9 = "SELECT * from obesity_health LIMIT 3"
 
 # Execute the SQL query and load the results into a DataFrame
 data9 = pd.read_sql(sql_query9, conn)
