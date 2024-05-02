e600e88bfa386d997935b7426a178fa4377f0994
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-01 22:09:51 -0700

    test

---
+++ loadPop.py
@@ -0,0 +1,119 @@
+import requests
+import pandas as pd
+import sqlite3
+
+
+# method to get population data as population
+def get_pop_data():
+    # get api data
+
+    key = "d57dfb3474ecd922cc1c04161f67290ae9f3c25d"
+    url2019 = f" https://api.census.gov/data/2019/pep/population?get=NAME,POP&for=state:*&key={key}"
+    url2018 = f" https://api.census.gov/data/2018/pep/population?get=GEONAME,POP&for=state:*&key={key}"
+    url2017 = f" https://api.census.gov/data/2017/pep/population?get=GEONAME,POP&for=state:*&key={key}"
+    url2016 = f" https://api.census.gov/data/2016/pep/population?get=GEONAME,POP&for=state:*&key={key}"
+    url2015 = f" https://api.census.gov/data/2015/pep/population?get=GEONAME,POP&for=state:*&key={key}"
+    url2014 = f" https://api.census.gov/data/2014/pep/natstprc?get=STNAME,POP&for=state:*&DATE_=6&key={key}"
+    url2013 = f" https://api.census.gov/data/2013/pep/natstprc?get=STNAME,POP&for=state:*&DATE_=6&key={key}"
+    urlxlsx = "https://www2.census.gov/programs-surveys/popest/tables/2020-2023/state/totals/NST-EST2023-POP.xlsx"
+
+    #2019
+    r_data2019 = requests.get(url2019).json()
+    df_2019 = pd.DataFrame(r_data2019[1:], columns=r_data2019[0])
+    df_2019.drop(['state'], axis=1, inplace=True)
+    df_2019['POP'].apply(lambda x: int(x))
+    df_2019.rename(columns = {'NAME': 'Region', 'POP':2019}, inplace = True)
+    df_2019.set_index('Region')
+
+    #2018
+    r_data2018 = requests.get(url2018).json()
+    df_2018 = pd.DataFrame(r_data2018[1:], columns=r_data2018[0])
+    df_2018.drop(['state'], axis=1, inplace=True)
+    df_2018['POP'].apply(lambda x: int(x))
+    df_2018.rename(columns = {'GEONAME': 'Region', 'POP':2018}, inplace = True)
+    df_2018.set_index('Region')
+
+    #2017
+    r_data2017 = requests.get(url2017).json()
+    df_2017 = pd.DataFrame(r_data2017[1:], columns=r_data2017[0])
+    df_2017.drop(['state'], axis=1, inplace=True)
+    df_2017['POP'].apply(lambda x: int(x))
+    df_2017.rename(columns = {'GEONAME': 'Region', 'POP':2017}, inplace = True)
+    df_2017.set_index('Region')
+
+    #2016
+    r_data2016 = requests.get(url2016).json()
+    df_2016 = pd.DataFrame(r_data2016[1:], columns=r_data2016[0])
+    df_2016.drop(['state'], axis=1, inplace=True)
+    df_2016['POP'].apply(lambda x: int(x))
+    df_2016.rename(columns = {'GEONAME': 'Region', 'POP':2016}, inplace = True)
+    df_2016.set_index('Region')
+
+    #2015
+    r_data2015 = requests.get(url2015).json()
+    df_2015 = pd.DataFrame(r_data2015[1:], columns=r_data2015[0])
+    df_2015.drop(['state'], axis=1, inplace=True)
+    df_2015['POP'].apply(lambda x: int(x))
+    df_2015['GEONAME'] = df_2015['GEONAME'].apply(lambda x: x.split(',')[0])
+    df_2015.rename(columns = {'GEONAME': 'Region', 'POP':2015}, inplace = True)
+    df_2015.set_index('Region')
+
+    #2014
+    r_data2014 = requests.get(url2014).json()
+    df_2014 = pd.DataFrame(r_data2014[1:], columns=r_data2014[0])
+    df_2014.drop('DATE_', axis = 1, inplace = True)
+    df_2014.drop(['state'], axis=1, inplace=True)
+    df_2014['POP'].apply(lambda x: int(x))
+    df_2014['STNAME'] = df_2014['STNAME'].apply(lambda x: x.replace('Puerto Rico Commonwealth', 'Puerto Rico'))
+    df_2014.rename(columns = {'STNAME': 'Region', 'POP':2014}, inplace = True)
+    df_2014.set_index('Region')
+
+    #2013
+    r_data2013 = requests.get(url2013).json()
+    df_2013 = pd.DataFrame(r_data2013[1:], columns=r_data2013[0])
+    df_2013.drop(['state'], axis=1, inplace=True)
+    df_2013.drop('DATE_', axis = 1, inplace = True)
+    df_2013['POP'].apply(lambda x: int(x))
+    df_2013['STNAME'] = df_2013['STNAME'].apply(lambda x: x.replace('Puerto Rico Commonwealth', 'Puerto Rico'))
+    df_2013.rename(columns = {'STNAME': 'Region', 'POP':2013}, inplace = True)
+    df_2013.set_index('Region')
+
+    df1 = pd.read_excel(urlxlsx, header = 3, nrows = 58)
+
+    df1.rename(columns = {df1.columns[0]:'Region', df1.columns[1]:'Base'}, inplace = True)
+    df1.dropna(inplace=True)
+    df1.drop('Base', axis=1, inplace=True)
+    df1['Region'] = df1['Region'].apply(lambda x: x.replace('.', ''))
+    df1.set_index('Region')
+
+    df1[2020] = df1[2020].apply(lambda x: int(x))
+    df1[2021] = df1[2021].apply(lambda x: int(x))
+    df1[2022] = df1[2022].apply(lambda x: int(x))
+    df1[2023] = df1[2023].apply(lambda x: int(x))
+
+    data = pd.merge(df_2013, df_2014)
+    data = pd.merge(data, df_2015)
+    data = pd.merge(data, df_2016)
+    data = pd.merge(data, df_2017)
+    data = pd.merge(data, df_2018)
+    data = pd.merge(data, df_2019)
+    data = pd.merge(data, df1)
+
+    def feed_api_data(data, table_name):
+        conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        cur = conn.cursor()
+
+        
+        # set to sql table
+        data.to_sql(table_name, conn, if_exists='replace')
+
+        ### Your code ends here ###
+        
+        cur.execute(f'SELECT * FROM {table_name} LIMIT 2')
+        return_val = cur.fetchall()
+        print(return_val)
+
+        conn.commit()
+        conn.close()
+
+    feed_api_data(data, "population")
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import loadData
 import pandas as pd
 import sqlite3
 import loadObesity
+import loadPop
 
 
 
@@ -12,13 +13,13 @@ print("hi")
 
 print("data loaded")
 
-#loadObesity.get_obesity_data()
+#loadPop.get_pop_data()
 
 conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 cur = conn.cursor()
 
 
-sql_query9 = "SELECT * from obesity_health LIMIT 3"
+sql_query9 = "SELECT * from population LIMIT 3"
 
 # Execute the SQL query and load the results into a DataFrame
 data9 = pd.read_sql(sql_query9, conn)
