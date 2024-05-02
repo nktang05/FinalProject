abe434c0e842cf3b0ea27aed2cbac0e8f65afbea
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-02 03:09:00 -0700

    test

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import sqlite3
 import loadData
 import menu
+import grocery
+import obesity
 
 
 import matplotlib.pyplot as plt
@@ -12,96 +14,29 @@ from bs4 import BeautifulSoup
 
 #loadData.loadAllData()
 
-def locationString(location):
-    conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-    cur = conn.cursor()
-    
-    if (location == "West" or location == "South" or location == "Northeast" or location == "Midwest"):
-        # Query to retrieve the states in the West region
-        sql_query9 = """
-            SELECT Name
-            FROM states
-            WHERE Region = ?
-        """
-    
-        # Execute the SQL query to retrieve the states in the West region
-        region = pd.read_sql(sql_query9, conn, params=(location,))
-        
-        # List of state names in the West region
-        states = region['Name'].tolist()
-        
-        # Convert the list of state names to a comma-separated string for the IN clause
-        state_names_str = ', '.join([f"'{state}'" for state in states])
 
-    else:
-        state_names_str = f"'{location}'"
+selectFood, selectRegion = menu.menugrocery()
+seriesDict = grocery.getSeriesId(selectFood, selectRegion)
+optionsList = list(seriesDict.values())
+option = st.selectbox(
+    'What option would you like?',
+    optionsList)
+selected_key = next(key for key, value in seriesDict.items() if value == option)
+df_table = grocery.grocery(selected_key, seriesDict)
 
-    # Close the connection
-    conn.close()
+data1 = obesity.obesityRegion(selectRegion)
 
-    return state_names_str
+fig, ax = plt.subplots()
 
-# to compare regions
-def obesityRegion(location1, location2, yearStart = None, yearEnd = None):
-    # Connect to the database
-    conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-    cur = conn.cursor()
+line1, = ax.plot(df_table['Year'], df_table['Price'], linestyle = "--", color = "blue", label='Price')
+ax.set_xlabel('Years')
+ax.set_ylabel('Data1')
 
-    state_names_str1 = locationString(location1)
-    state_names_str2 = locationString(location2)
+ax2 = ax.twinx()
+line2,  = ax2.plot(data1['Year'], data1['Avg_Obesity_Rate'], label='Obesity Rate', color = "red")
+ax2.set_ylabel('Data2')
 
-    # Condition for filtering years
-    year_condition = ""
-    if yearStart is not None and yearEnd is not None:
-        year_condition = f"AND YearEnd >= {yearStart} AND YearEnd <= {yearEnd}"
+ax2.legend(handles=[line1, line2])
+plt.title('2 plots')
 
-    # SQL query to calculate the average data value for each year in the West region
-    sql_query2 = f"""
-        SELECT YearEnd AS Year, AVG(data_value) AS Avg_Obesity_Rate
-        FROM obesity_health o
-        JOIN states s ON o.locationDesc = s.Name
-        WHERE s.Name IN ({state_names_str1}) AND o.class = 'Obesity / Weight Status' {year_condition}
-        GROUP BY YearEnd
-    """
-    
-    # Execute the SQL query and load the results into a DataFrame
-    data2 = pd.read_sql(sql_query2, conn)
-
-    #SQL query to calculate the average data value for each year in the West region
-    sql_query3 = f"""
-        SELECT YearEnd AS Year, AVG(data_value) AS Avg_Obesity_Rate
-        FROM obesity_health o
-        JOIN states s ON o.locationDesc = s.Name
-        WHERE s.Name IN ({state_names_str2}) AND o.class = 'Obesity / Weight Status' {year_condition}
-        GROUP BY YearEnd
-    """
-    
-    # Execute the SQL query and load the results into a DataFrame
-    data3 = pd.read_sql(sql_query3, conn)
-    
-    # Print the results
-    print(data2)
-
-    st.write("test")
-    
-    # Plot the data
-    fig = plt.figure()
-    plt.plot(data2['Year'], data2['Avg_Obesity_Rate'], label=location1)
-    plt.plot(data3['Year'], data3['Avg_Obesity_Rate'], label=location2)
-
-
-    # Add labels and title
-    plt.xlabel('Year')
-    plt.ylabel('Obestiy Rate')
-    plt.title(f"Average Obesity Rated in {location1} vs {location2} from {yearStart} to {yearEnd}")
-
-    # Add legend
-    plt.legend()
-    plt.tight_layout()  # Adjust layout to prevent clipping of x-axis labels
-    st.pyplot(fig)
-
-    # Close the connection
-    conn.close()
-
-year, region1, region2 = menu.menuObestiyRegion()
-obesityRegion(region1, region2, year[0], year[1])
+st.pyplot(fig)
--- obesity.py
+++ obesity.py
@@ -9,6 +9,8 @@ import sqlite3
 import pandas as pd
 import streamlit as st
 
+
+# gives outlook on entire country
 def obesityTable(year1, year2):
 
     conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
@@ -45,3 +47,125 @@ def obesityTable(year1, year2):
 
 year1, year2 = menu.menuObesity()
 obesityTable(year1, year2)
+
+
+
+
+def locationString(location):
+    conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    cur = conn.cursor()
+    
+    if (location == "West" or location == "South" or location == "Northeast" or location == "Midwest"):
+        # Query to retrieve the states in the West region
+        sql_query9 = """
+            SELECT Name
+            FROM states
+            WHERE Region = ?
+        """
+    
+        # Execute the SQL query to retrieve the states in the West region
+        region = pd.read_sql(sql_query9, conn, params=(location,))
+        
+        # List of state names in the West region
+        states = region['Name'].tolist()
+        
+        # Convert the list of state names to a comma-separated string for the IN clause
+        state_names_str = ', '.join([f"'{state}'" for state in states])
+
+    else:
+        state_names_str = f"'{location}'"
+
+    # Close the connection
+    conn.close()
+
+    return state_names_str
+
+# to compare regions
+def obesityRegionCompare(location1, location2, yearStart = None, yearEnd = None):
+    # Connect to the database
+    conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    cur = conn.cursor()
+
+    state_names_str1 = locationString(location1)
+    state_names_str2 = locationString(location2)
+
+    # Condition for filtering years
+    year_condition = ""
+    if yearStart is not None and yearEnd is not None:
+        year_condition = f"AND YearEnd >= {yearStart} AND YearEnd <= {yearEnd}"
+
+    # SQL query to calculate the average data value for each year in the West region
+    sql_query2 = f"""
+        SELECT YearEnd AS Year, AVG(data_value) AS Avg_Obesity_Rate
+        FROM obesity_health o
+        JOIN states s ON o.locationDesc = s.Name
+        WHERE s.Name IN ({state_names_str1}) AND o.class = 'Obesity / Weight Status' {year_condition}
+        GROUP BY YearEnd
+    """
+    
+    # Execute the SQL query and load the results into a DataFrame
+    data2 = pd.read_sql(sql_query2, conn)
+
+    #SQL query to calculate the average data value for each year in the West region
+    sql_query3 = f"""
+        SELECT YearEnd AS Year, AVG(data_value) AS Avg_Obesity_Rate
+        FROM obesity_health o
+        JOIN states s ON o.locationDesc = s.Name
+        WHERE s.Name IN ({state_names_str2}) AND o.class = 'Obesity / Weight Status' {year_condition}
+        GROUP BY YearEnd
+    """
+    
+    # Execute the SQL query and load the results into a DataFrame
+    data3 = pd.read_sql(sql_query3, conn)
+    
+    # Print the results
+    print(data2)
+    
+    # Plot the data
+    fig = plt.figure()
+    plt.plot(data2['Year'], data2['Avg_Obesity_Rate'], label=location1)
+    plt.plot(data3['Year'], data3['Avg_Obesity_Rate'], label=location2)
+
+
+    # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Obestiy Rate')
+    plt.title(f"Average Obesity Rated in {location1} vs {location2} from {yearStart} to {yearEnd}")
+
+    # Add legend
+    plt.legend()
+    plt.tight_layout()  # Adjust layout to prevent clipping of x-axis labels
+    st.pyplot(fig)
+
+    # Close the connection
+    conn.close()
+
+year, region1, region2 = menu.menuObestiyRegion()
+obesityRegionCompare(region1, region2, year[0], year[1])
+
+
+def obesityRegion(location1, yearStart = None, yearEnd = None):
+    # Connect to the database
+    conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+    cur = conn.cursor()
+
+    state_names_str1 = locationString(location1)
+
+    # Condition for filtering years
+    year_condition = ""
+    if yearStart is not None and yearEnd is not None:
+        year_condition = f"AND YearEnd >= {yearStart} AND YearEnd <= {yearEnd}"
+
+    # SQL query to calculate the average data value for each year in the West region
+    sql_query2 = f"""
+        SELECT YearEnd AS Year, AVG(data_value) AS Avg_Obesity_Rate
+        FROM obesity_health o
+        JOIN states s ON o.locationDesc = s.Name
+        WHERE s.Name IN ({state_names_str1}) AND o.class = 'Obesity / Weight Status' {year_condition}
+        GROUP BY YearEnd
+    """
+    
+    # Execute the SQL query and load the results into a DataFrame
+    data2 = pd.read_sql(sql_query2, conn)
+
+    return data2
