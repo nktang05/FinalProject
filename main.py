3d298294cc4c45da17d61c6bf882c02afd26efba
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-02 11:51:41 -0700

    test

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ from bs4 import BeautifulSoup
 graph = menu.menuUnion()
 
 selectFood, selectRegion = menu.menugrocery()
+selectFood, selectRegion = menu.menugrocery()
+
 seriesDict = grocery.getSeriesId(selectFood, selectRegion)
 optionsList = list(seriesDict.values())
 option = st.selectbox(
