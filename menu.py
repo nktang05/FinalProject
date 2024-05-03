c0622d487c4686569038965c84767d2288f3cb67
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-03 01:02:34 -0700

    test

--- menu.py
+++ menu.py
@@ -1,24 +1,17 @@
 import streamlit as st
 
-
-
+# method to get user grocery requests
 def menugrocery():
-    #foodType = ["beef", "chicken", "bread", "apples", "candy", "sugar", "ice cream"]
+    # list of possible regions
     region = ["Northeast", "West", "South", "Midwest"]
 
-    
+    # get use region
     selectRegion = st.selectbox(
         'Select a region?',
         region, placeholder = "Northeast")
     st.write("Your chosen region", selectRegion)
 
-    """
-    selectFood = st.selectbox(
-        'Select a item?',
-        foodType, placeholder = "beef")
-    st.write("Your chosen food item", selectFood)
-    """
-    
+    # get user food
     selectFood = st.text_input('Select Food Type', 'beef')
     st.write("Your chosen food item is", selectFood)
 
@@ -26,17 +19,18 @@ def menugrocery():
 
     return selectFood, selectRegion
 
-
-    
-
+# method to get user obesity requests
 def menuObesity():
+    # list of year options
     years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
 
+    # get user year 1
     selectYear1 = st.selectbox(
         'Select a year to compare?',
         years)
     st.write("Your chosen year", selectYear1)
 
+    # get user year 2
     selectYear2 = st.selectbox(
         'Select a second year to compare?',
         years)
@@ -44,38 +38,47 @@ def menuObesity():
     
     return selectYear1, selectYear2
 
-
+# mthod to get population user input
 def menuPop():
+    # valid year options
     year = st.slider('Select a range of years',
     2013, 2023, (2013, 2023))
-
+    # valid region options
     region = ["Northeast", "West", "South", "Midwest", 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'National', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
    
+    # ger user region 1
     selectRegion1 = st.selectbox('Select the first region or state', region)
     st.write("You selected:", selectRegion1)
-
+    # get user  region 2
     selectRegion2 = st.selectbox('Select the second region or state', region)
     st.write("You selected:", selectRegion2)
 
     return year, selectRegion1, selectRegion2
 
+# method to get obesity user input
 def menuObestiyRegion():
-
+    # valuid year options
     year = st.slider('Select a range of years',
     2011, 2022, (2011, 2022))
 
+    # valid region population
     region = ["Northeast", "West", "South", "Midwest", 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'National', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
    
+    # get user region 1
     selectRegion1 = st.selectbox('Select the first region or state', region)
     st.write("You selected:", selectRegion1)
 
+    # get user region 2
     selectRegion2 = st.selectbox('Select the second region or state', region)
     st.write("You selected:", selectRegion2)
 
     return year, selectRegion1, selectRegion2
 
+# method to get combined graph user input
 def menuUnion():
+    # optional choices
     choice = ["Food Prices and Obesity Rates", "Food Prices and Population", "Population and Obesity Rates"]
+    # let user pick graph
     graph = st.selectbox('Select your comparison graph', choice)
     st.write("You selected:", graph)
 
--- obesity.py
+++ obesity.py
@@ -1,6 +1,4 @@
 import menu
-
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 import requests
@@ -9,15 +7,11 @@ import sqlite3
 import pandas as pd
 import streamlit as st
 
-
-# gives outlook on entire country
+# method to graph obesity data and compare
 def obesityTable(year1, year2):
-
     conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 
-    ## Your code here
-
-    # Construct SQL queries with placeholders for years
+    # Cuery data
     sql_query2 = f"SELECT AVG(data_value) AS '{year1}', LocationDesc FROM obesity_health WHERE YearEnd = ? AND class = 'Obesity / Weight Status' GROUP BY LocationDesc Order By LocationDesc ASC"
     sql_query3 = f"SELECT AVG(data_value) AS '{year2}', LocationDesc FROM obesity_health WHERE YearEnd = ? AND class = 'Obesity / Weight Status' GROUP BY LocationDesc Order By LocationDesc ASC"
 
@@ -28,6 +22,7 @@ def obesityTable(year1, year2):
     conn.commit()
     conn.close()
 
+    # plot figure
     fig = plt.figure()
     plt.plot(data2['LocationDesc'], data2[str(year1)], marker='o', linestyle="", label=str(year1))
     plt.plot(data3['LocationDesc'], data3[str(year2)], marker='o', linestyle="", label=str(year2))
@@ -45,16 +40,12 @@ def obesityTable(year1, year2):
     plt.tight_layout()  # Adjust layout to prevent clipping of x-axis labels
     st.pyplot(fig)
 
-year1, year2 = menu.menuObesity()
-obesityTable(year1, year2)
-
-
-
-
+# method to get region from states
 def locationString(location):
     conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
     cur = conn.cursor()
     
+    # query the state names fron user data
     if (location == "West" or location == "South" or location == "Northeast" or location == "Midwest"):
         # Query to retrieve the states in the West region
         sql_query9 = """
@@ -80,21 +71,22 @@ def locationString(location):
 
     return state_names_str
 
-# to compare regions
+# method to compare regions of obesity rates
 def obesityRegionCompare(location1, location2, yearStart = None, yearEnd = None):
     # Connect to the database
     conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
     cur = conn.cursor()
 
+    # get string of states in region
     state_names_str1 = locationString(location1)
     state_names_str2 = locationString(location2)
 
-    # Condition for filtering years
+    # Condition for years
     year_condition = ""
     if yearStart is not None and yearEnd is not None:
         year_condition = f"AND YearEnd >= {yearStart} AND YearEnd <= {yearEnd}"
 
-    # SQL query to calculate the average data value for each year in the West region
+    # query to get obesity data using join of tables
     sql_query2 = f"""
         SELECT YearEnd AS Year, AVG(data_value) AS Avg_Obesity_Rate
         FROM obesity_health o
@@ -103,10 +95,10 @@ def obesityRegionCompare(location1, location2, yearStart = None, yearEnd = None)
         GROUP BY YearEnd
     """
     
-    # Execute the SQL query and load the results into a DataFrame
+    # Execute the SQL query
     data2 = pd.read_sql(sql_query2, conn)
 
-    #SQL query to calculate the average data value for each year in the West region
+    #SQL query for next set of data
     sql_query3 = f"""
         SELECT YearEnd AS Year, AVG(data_value) AS Avg_Obesity_Rate
         FROM obesity_health o
@@ -118,37 +110,29 @@ def obesityRegionCompare(location1, location2, yearStart = None, yearEnd = None)
     # Execute the SQL query and load the results into a DataFrame
     data3 = pd.read_sql(sql_query3, conn)
     
-    # Print the results
-    #print(data2)
-    
     # Plot the data
     fig = plt.figure()
     plt.plot(data2['Year'], data2['Avg_Obesity_Rate'], label=location1)
     plt.plot(data3['Year'], data3['Avg_Obesity_Rate'], label=location2)
 
-
-    # Add labels and title
     plt.xlabel('Year')
     plt.ylabel('Obestiy Rate')
     plt.title(f"Average Obesity Rated in {location1} vs {location2} from {yearStart} to {yearEnd}")
 
-    # Add legend
     plt.legend()
-    plt.tight_layout()  # Adjust layout to prevent clipping of x-axis labels
+    plt.tight_layout() 
     st.pyplot(fig)
 
     # Close the connection
     conn.close()
 
-year, region1, region2 = menu.menuObestiyRegion()
-obesityRegionCompare(region1, region2, year[0], year[1])
-
-
+# method to get obesity by yers
 def obesityRegion(location1, yearStart = None, yearEnd = None):
     # Connect to the database
     conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
     cur = conn.cursor()
 
+    # method to get location
     state_names_str1 = locationString(location1)
 
     # Condition for filtering years
@@ -156,7 +140,7 @@ def obesityRegion(location1, yearStart = None, yearEnd = None):
     if yearStart is not None and yearEnd is not None:
         year_condition = f"AND YearEnd >= {yearStart} AND YearEnd <= {yearEnd}"
 
-    # SQL query to calculate the average data value for each year in the West region
+    # SQL query 
     sql_query2 = f"""
         SELECT YearEnd AS Year, AVG(data_value) AS Avg_Obesity_Rate
         FROM obesity_health o
